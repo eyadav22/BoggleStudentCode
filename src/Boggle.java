28879d29dac6afe26c54187790bc09b3803820e1
emill-y <eishayadav@gmail.com>
2026-01-07 14:10:02 -0800

    finished to-do as well as added TST class fixes

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,36 +1,76 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Boggle {
 
     public static String[] findWords(char[][] board, String[] dictionary) {
 
-        ArrayList<String> goodWords = new ArrayList<String>();
-
-        // TODO: Complete the function findWords(). Add all words that are found both on the board
-        //  and in the dictionary.
-
-        // View the board as a series of nodes, then start at one node and traverse
-        // using depth first search inorder traversal, adding all "words", even if not real, to a list
-        // use recursive depth first search to keep track of visiting
-        // if visited then mark as visited, but recursion allows to be unvisited
-        // Then lookup each word in a TST made from the dictionary of words
-        // Need seperate TST class
-        // if word present in the TST, then add to the arraylist goodWords
-
-        // Build TST of words in the dictionary
+        // Initialize new TST to store dictionary
         TST tst = new TST();
         for (String word : dictionary) {
             tst.insert(word);
         }
 
-        // Use dfs to check all words
+        int rows = board.length;
+        int cols = board[0].length;
 
+        boolean[][] visited = new boolean[rows][cols];
+        // Use a HashSet instead of an array list to avoid duplicates
+        HashSet<String> goodWords = new HashSet<>();
 
-        // Convert the list into a sorted array of strings, then return the array.
-        String[] sol = new String[goodWords.size()];
-        goodWords.toArray(sol);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dfs(board, visited, i, j, "", tst.getRoot(), goodWords);
+            }
+        }
+        String[] sol = goodWords.toArray(new String[0]);
         Arrays.sort(sol);
         return sol;
+    }
+
+    private static void dfs(char[][] board, boolean[][] visited, int i, int j, String prefix, TST.TSTNode node, HashSet<String> goodWords) {
+        // Return if out of bounds
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return;
+        }
+        // Return if previously visited
+        if (visited[i][j]) return;
+
+        char c = board[i][j];
+        node = nextNode(node, c);
+
+        // Return if node is null
+        if (node == null) return;
+
+        // Optimize with numbers (To-do)
+        String word = prefix + c;
+        // Add word if present in Dictionary
+        if (node.isEndOfWord) {
+            goodWords.add(word);
+            // Allows us the remove duplicates without adding additional time complexity
+            // Makes HashSet redundant
+            node.isEndOfWord = false;
+        }
+        // Mark node as visited
+        visited[i][j] = true;
+        // Recurse to adjacent squares
+        dfs(board, visited, i - 1, j, word, node.mid, goodWords);
+        dfs(board, visited, i + 1, j, word, node.mid, goodWords);
+        dfs(board, visited, i, j - 1, word, node.mid, goodWords);
+        dfs(board, visited, i, j + 1, word, node.mid, goodWords);
+        // Mark node as unvisited so it can be used in a future word
+        visited[i][j] = false;
+    }
+
+    // Helper Method for recursion
+    private static TST.TSTNode nextNode(TST.TSTNode node, char c) {
+        // Allows for integration between dfs and TST
+        while (node != null) {
+            if (c < node.c) node = node.left;
+            else if (c > node.c) node = node.right;
+            else return node;
+        }
+        return null;
     }
 }
--- src/TST.java
+++ src/TST.java
@@ -1,51 +1,75 @@
 public class TST {
-    private class TSTNode {
-        // Current Char
+
+    // Make Node accessible to Boggle
+    public class TSTNode {
         char c;
-        // Three possible paths down
         TSTNode left, mid, right;
-        // Marks valid words from sub-words
         boolean isEndOfWord;
     }
 
     private TSTNode root;
 
-    // Inserts words at the beginning
     public void insert(String word) {
-        if (word == null || word.isEmpty()) {return;}
+        if (word == null || word.isEmpty()) return;
         root = insert(root, word.toLowerCase(), 0);
     }
 
-    // Inserts words at specific points
     private TSTNode insert(TSTNode node, String word, int index) {
         char c = word.charAt(index);
+
         if (node == null) {
             node = new TSTNode();
             node.c = c;
         }
-        // Three paths down
-        if (c < node.c) {node.left = insert(node.left, word, index);}
-        else if (c > node.c) { node.right = insert(node.right, word, index);}
-        else if (index < word.length() - 1) {node.mid = insert(node.mid, word, index + 1);}
-        else {node.isEndOfWord = true;}
+
+        if (c < node.c) {
+            node.left = insert(node.left, word, index);
+        } else if (c > node.c) {
+            node.right = insert(node.right, word, index);
+        } else if (index < word.length() - 1) {
+            node.mid = insert(node.mid, word, index + 1);
+        } else {
+            node.isEndOfWord = true;
+        }
+
         return node;
     }
 
     public boolean contains(String word) {
-        if (word == null || word.isEmpty()) {return false;}
+        if (word == null || word.isEmpty()) return false;
         TSTNode node = get(root, word.toLowerCase(), 0);
         return node != null && node.isEndOfWord;
     }
 
     private TSTNode get(TSTNode node, String word, int index) {
-        if (node == null) {return null;}
+        if (node == null) return null;
+
         char c = word.charAt(index);
-        // Three paths down
-        // Char is less than the current node
-        if (c < node.c) {return get(node.left, word, index);}
-        // Char is greater than the current node
-        else if (c > node.c) {return get(node.right, word, index);}
-        else if (index < word.length() - 1) {return get(node.mid, word, index + 1);}
-        else {return node;}
+
+        if (c < node.c) {
+            return get(node.left, word, index);
+        } else if (c > node.c) {
+            return get(node.right, word, index);
+        } else if (index < word.length() - 1) {
+            return get(node.mid, word, index + 1);
+        } else {
+            return node;
+        }
+    }
+
+
+    // Needed to start DFS
+    public TSTNode getRoot() {
+        return root;
+    }
+
+    // Move ONE character forward in the TST
+    public TSTNode nextNode(TSTNode node, char c) {
+        while (node != null) {
+            if (c < node.c) node = node.left;
+            else if (c > node.c) node = node.right;
+            else return node;
+        }
+        return null;
     }
 }
